0a59230bbce398468fa1a13f2b1287eefb1e9937
Shamim Ahmed <wdshamim822@gmail.com>
2022-11-30 21:25:44 +0600

    Empty-Commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 4,
+//     speed: 500,
+//     slidesPerGroup: 4,
+//     spaceBetween: 20,
+//     breakpoints: {
+//       320:{
+//         slidesPerView: 1,
+//         slidesPerGroup: 1,
+//       },
+//       480:{
+//         slidesPerView: 2,
+//         slidesPerGroup: 2,
+//       },
+//       576:{
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//         slidesPerGroup: 2,
+//       },
+//       768:{
+//         slidesPerView: 3,
+//         slidesPerGroup: 3,
+//       },
+//       992:{
+//         slidesPerView: 4,
+//         slidesPerGroup: 4,
+//       }
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     }
+//   });
